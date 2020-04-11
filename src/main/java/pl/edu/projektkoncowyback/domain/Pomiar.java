31aeace11b0f74d7a86b15606404e8a9e3b18058
KUBA <jmikulsk92>
2020-04-11 11:36:37 +0200

    make it better

--- src/main/java/pl/edu/projektkoncowyback/domain/Pomiar.java
+++ src/main/java/pl/edu/projektkoncowyback/domain/Pomiar.java
@@ -14,10 +14,10 @@ public class Pomiar {
     @GeneratedValue(strategy = GenerationType.IDENTITY) Integer id;
 
 
-    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    private Date tymczasowa = new Date();
-	private String data = dateFormat.format(tymczasowa);
-//private String data;
+//    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+//    private Date tymczasowa = new Date();
+//	private String data = dateFormat.format(tymczasowa);
+private String data;
     private Long temperatura;
     private Long wilgotnosc;
     private String info;
